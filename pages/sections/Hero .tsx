88b5d7167177a0b266709a6375797fb382c04008
Sotorev <73616133+Sotorev@users.noreply.github.com>
2022-12-30 22:13:52 -0600

    Entities fixed

--- pages/sections/Hero .tsx
+++ pages/sections/Hero .tsx
@@ -51,7 +51,7 @@ function Hero() {
 					idx={"Hello, I'm Manuel,".length}
 				/>
 				<StyledP className='animate__animated animate__fadeInUp' >
-					I'm a web developer. I'm passionate about
+					I&apos;m a web developer. I&apos;m passionate about
 					creating beautiful and functional websites.
 				</StyledP>
 			</TextWrapper>
